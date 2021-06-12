e10a7e1e634ed3a92574d198edde50ee2bd08589
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-06-12 21:03:29 +0100

    update

---
+++ src/main/java/com/shun/dao/BaseDao.java
@@ -0,0 +1,101 @@
+package com.shun.dao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+//操作数据库的公共类
+public class BaseDao {
+    private static String driver;
+    private static String url;
+    private static String username;
+    private static String password;
+
+    //静态代码块，当类加载的时候自动初始化
+    static {
+        Properties properties = new Properties();
+        //通过类加载器获取资源
+        InputStream is = BaseDao.class.getClassLoader().getResourceAsStream("db.properties");
+
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        driver = properties.getProperty("driver");
+        url = properties.getProperty("url");
+        username = properties.getProperty("username");
+        password = properties.getProperty("password");
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+            connection = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static ResultSet exectue(Connection connection, String sql, Object[] params, ResultSet resultSet, PreparedStatement preparedStatement) throws Exception {
+        preparedStatement =connection.prepareStatement(sql);
+
+        for(int i = 0; i < params.length; i++) {
+            preparedStatement.setObject(i + 1, params[i]);
+        }
+
+        resultSet = preparedStatement.executeQuery();
+        return resultSet;
+    }
+
+    public static int exectue(Connection connection, String sql, Object[] params, PreparedStatement preparedStatement) throws Exception {
+        preparedStatement =connection.prepareStatement(sql);
+
+        for(int i = 0; i < params.length; i++) {
+            preparedStatement.setObject(i + 1, params[i]);
+        }
+
+        int updateRows = preparedStatement.executeUpdate();
+        return updateRows;
+    }
+
+    public static boolean closeResource(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet) {
+        boolean flag = true;
+
+        if(resultSet != null) {
+            try {
+                resultSet.close();
+                resultSet = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                flag = false;
+            }
+        }
+
+        if(preparedStatement != null) {
+            try {
+                preparedStatement.close();
+                preparedStatement = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                flag = false;
+            }
+        }
+
+        if(connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                flag = false;
+            }
+        }
+
+        return flag;
+    }
+}
